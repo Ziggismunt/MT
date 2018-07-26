60731764188502ea5d01ce4cfcddde9923e2027f
Ekaterina Oskolkova <Ekaterina_Oskolkova@epam.com>
2018-07-26 15:15:17 +0300

    First Commit

---
+++ src/main/java/com/mt/oep/OepApplication.java
@@ -0,0 +1,12 @@
+package com.mt.oep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OepApplication.class, args);
+	}
+}
